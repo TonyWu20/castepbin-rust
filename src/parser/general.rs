6296724deda3a81ca0f49f0dd8a703dea48635e1
TonyWu20 <tony.w21@gmail.com>
2022-06-24 13:01:41 +0800

    Add parse_u8 util

--- src/parser/general.rs
+++ src/parser/general.rs
@@ -4,7 +4,7 @@ use nom::{
     character::complete::{char, multispace1, one_of},
     combinator::{map_res, opt, recognize},
     multi::{count, length_data, many0, many1},
-    number::complete::{be_f32, be_f64, be_i32, be_u32},
+    number::complete::{be_f32, be_f64, be_i32, be_u32, be_u8},
     sequence::{preceded, terminated, tuple},
     IResult,
 };
@@ -23,6 +23,26 @@ fn parse_record_and_data(data: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
 pub fn parse_data(data: &[u8]) -> IResult<&[u8], &[u8]> {
     preceded(parse_record, parse_record)(data)
 }
+pub fn parse_u8_vec_from_u32_vec_record(data: &[u8]) -> IResult<&[u8], Vec<u8>> {
+    let (i, u32_data) = parse_record(data)?;
+    let (_, u32_array) = many0(parse_u32)(u32_data)?;
+    let u8_vec = u32_array
+        .into_iter()
+        .map(|v| -> u8 {
+            v.to_string()
+                .parse::<u8>()
+                .expect(&format!("Parse to u8 error {}", v))
+        })
+        .collect();
+    Ok((i, u8_vec))
+}
+pub fn parse_u8_from_record(data: &[u8]) -> IResult<&[u8], u8> {
+    let (i, u32_data) = parse_record(data)?;
+    let (_, u32_val) = parse_u32(u32_data)?;
+    let u8_val: u8 = u32_val.to_string().parse::<u8>().unwrap();
+    Ok((i, u8_val))
+}
+
 pub fn parse_u32_from_record(data: &[u8]) -> IResult<&[u8], u32> {
     let (i, u32_data) = parse_record(data)?;
     Ok((i, parse_u32(u32_data).unwrap().1))
