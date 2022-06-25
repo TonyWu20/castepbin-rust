57816a52507cf8a31991c130ff95023be8dc61fc
TonyWu20 <tony.w21@gmail.com>
2022-06-26 04:59:12 +0800

    Benchmark

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,12 @@
+use castepbin_rust::dos::dos_util::calculate_total_dos_from_bands;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let band_file = "/Users/tonywu/Library/Mobile Documents/com~apple~CloudDocs/Programming/castepbin-rust/Pt_310_12lyr_v20_CO_DOS/Pt_310_12lyr_v20_CO_DOS.bands";
+    c.bench_function("calculate total dos", |b| {
+        b.iter(|| calculate_total_dos_from_bands(band_file, None, None))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
