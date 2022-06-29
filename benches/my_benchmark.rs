a463fd372e467e88cb3007a91b09e466127e24f8
TonyWu20 <tony.w21@gmail.com>
2022-06-29 12:00:44 +0800

    bench

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let band_file = "/Users/tonywu/Library/Mobile Documents/com~apple~CloudDocs/Programming/castepbin-rust/Pt_310_12lyr_v20_CO_DOS/Pt_310_12lyr_v20_CO_DOS.bands";
-    c.bench_function("calculate total dos", |b| {
+    c.bench_function("total dos", |b| {
         b.iter(|| calculate_total_dos_from_bands(band_file, None, None))
     });
 }
