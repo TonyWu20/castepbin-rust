d1172be9e8321733512cabadfe1741ad7e2e1311
TonyWu20 <tony.w21@gmail.com>
2022-07-15 17:22:01 +0800

    Adjust `DOS` struct to `PDOS` and `TDOS` struct

--- src/dos/dos_util.rs
+++ src/dos/dos_util.rs
@@ -3,25 +3,29 @@ use std::fs;
 use crate::parser::bands::Bands;
 
 use super::{
-    dos_compute::{compute_energy_range, total_dos},
-    DOS,
+    dos_compute::{compute_energy_range, total_dos, HATREE_TO_EV},
+    TDOS,
 };
 
 pub fn calculate_total_dos_from_bands(
     bands_file: &str,
     energy_range_limit: Option<(f64, f64)>,
     e_num_per_ev: Option<u32>,
-) -> DOS {
+) -> TDOS {
     let bands_file_string = fs::read_to_string(bands_file).expect("Failed to open .bands file!");
     let bands_data = Bands::parse(&bands_file_string)
         .expect("Failed to parse .bands!")
         .1;
-    let (e_min, e_max) = energy_range_limit.unwrap_or((-25.00, 20.00));
+    let (e_min, e_max) = energy_range_limit.unwrap_or((
+        (bands_data.min_eigen_energy() - bands_data.e_fermi()[0]) * HATREE_TO_EV - 2.00,
+        (bands_data.max_eigen_energy() - bands_data.e_fermi()[0]) * HATREE_TO_EV + 2.00,
+    ));
+    println!("{}, {}", e_min, e_max);
     let num_per_ev = e_num_per_ev.unwrap_or(100);
     let num_points = ((e_max - e_min) * num_per_ev as f64) as u32;
     let energy_range = compute_energy_range(e_min, e_max, num_points);
     let total_dos = total_dos(&bands_data, &energy_range);
-    DOS::new(bands_data.num_spins(), num_points, energy_range, total_dos)
+    TDOS::new(bands_data.num_spins(), num_points, energy_range, total_dos)
 }
 
 #[cfg(test)]
@@ -38,13 +42,10 @@ fn test_total_dos() {
     let total_dos = calculate_total_dos_from_bands(band_file, None, None);
     let (e, spin1) = (
         total_dos.energy_range().to_vec(),
-        total_dos.dos().0.to_vec(),
+        total_dos.dos()[0].to_vec(),
     );
-    let spin2: Vec<f64> = total_dos
-        .dos()
-        .1
-        .as_ref()
-        .unwrap()
+    let spin2: Vec<f64> = total_dos.dos()[1]
+        .to_vec()
         .iter()
         .map(|x| -> f64 { x * -1.0 })
         .collect();
